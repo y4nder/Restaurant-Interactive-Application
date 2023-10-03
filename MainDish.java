810e45ee7ef0bae0570419fd155b6cd5ff28ba19
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2023-10-03 02:10:29 +0000

    first commit

---
+++ MainDish.java
@@ -0,0 +1,6 @@
+public class MainDish extends MenuItem{
+    public MainDish(String itemName, double itemPrice){
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+}
---
+++ MenuItem.java
@@ -0,0 +1,8 @@
+abstract class MenuItem{
+    protected String itemName;
+    protected double itemPrice;
+
+    public String toString(){
+        return itemName + " " + itemPrice;
+    }
+}
